0b2c37ad0146380ff5df50c5434efdd134941b0e
Matheus.Juliao <matheus.juliao@stoneridge.com>
2023-01-10 18:35:20 -0300

    Aula 21 - API Spring: Criar Curso (HTTP POST)

--- src/main/java/com/matheus/controller/CourseController.java
+++ src/main/java/com/matheus/controller/CourseController.java
@@ -2,8 +2,12 @@ package com.matheus.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheus.model.Course;
@@ -28,5 +32,14 @@ public class CourseController {
     @GetMapping //Seria mesma coisa que fazer @RequestMapping(method = ResquestMethod.GET)
     public List<Course> list() { //Criando um método para retonar uma lista utilizando o java útil
         return courseRepository.findAll(); //Select * from da table
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        //System.out.println(course.getName());
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED)
+        //     .body(courseRepository.save(course));
     }
 }
