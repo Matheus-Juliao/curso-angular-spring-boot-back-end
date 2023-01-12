0f8efbf3bb8c0e90406718069c5858a038ea0634
Matheus.Juliao <matheus.juliao@stoneridge.com>
2023-01-12 15:26:41 -0300

    Aula 26 - API Spring: Buscar por ID

--- src/main/java/com/matheus/controller/CourseController.java
+++ src/main/java/com/matheus/controller/CourseController.java
@@ -3,7 +3,9 @@ package com.matheus.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,13 @@ public class CourseController {
     @GetMapping //Seria mesma coisa que fazer @RequestMapping(method = ResquestMethod.GET)
     public List<Course> list() { //Criando um método para retonar uma lista utilizando o java útil
         return courseRepository.findAll(); //Select * from da table
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+            .map(rec -> ResponseEntity.ok().body(rec))
+            .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
