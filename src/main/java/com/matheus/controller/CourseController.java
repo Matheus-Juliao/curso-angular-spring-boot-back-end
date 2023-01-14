26eef460ef145252816b36da32d29499dace0631
Matheus.Juliao <matheus.juliao@stoneridge.com>
2023-01-14 15:54:32 -0300

    Aula 35 - Validações da API com Java Bean Validation

--- src/main/java/com/matheus/controller/CourseController.java
+++ src/main/java/com/matheus/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.matheus.model.Course;
 import com.matheus.repository.CourseRepository;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 
+@Validated //Ativar Java Bean ou Hibernate Validate
 @RestController //@RestController fala que essa classe tem um end point, tem os métodos post, get, delete put....
 @RequestMapping("/api/courses") //Expõe o end point
 @AllArgsConstructor //Cria o construtor com lombok
@@ -39,7 +44,7 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id) {
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
         return courseRepository.findById(id)
             .map(rec -> ResponseEntity.ok().body(rec))
             .orElse(ResponseEntity.notFound().build());
@@ -47,7 +52,7 @@ public class CourseController {
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course) {
+    public Course create(@RequestBody @Valid Course course) {
         //System.out.println(course.getName());
         return courseRepository.save(course);
         // return ResponseEntity.status(HttpStatus.CREATED)
@@ -55,7 +60,7 @@ public class CourseController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
         return courseRepository.findById(id)
             .map(recordFound -> {
                 recordFound.setName(course.getName());
@@ -67,7 +72,7 @@ public class CourseController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
         return courseRepository.findById(id)
         .map(recordFound -> {
             courseRepository.deleteById(id);
--- src/main/java/com/matheus/model/Course.java
+++ src/main/java/com/matheus/model/Course.java
@@ -1,11 +1,16 @@
 package com.matheus.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data; //O lombok Gera os Getters e Setters, Construtor, etc. O Data gera todos que existe no pacote.
 
 @Data  
@@ -17,10 +22,16 @@ public class Course {
     @JsonProperty("_id") //Anotação para transformar a variável com underscore. ex: _id
     //@JsonIgnore Serve para ignorar
     private Long id;
-
-    @Column(name = "name", length = 200, nullable = false) //Consegue especificar o nome da tabela no banco, tamanho do banco e se o campo é not null
+    
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    @Column(name = "name", length = 100, nullable = false) //Consegue especificar o nome da tabela no banco, tamanho do banco e se o campo é not null
     private String name;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
 
